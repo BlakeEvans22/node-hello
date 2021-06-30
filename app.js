6c63936ec8ff69d7e5c07c73da7070eb63ecb19e
Blake Evans <32441662+BlakeEvans22@users.noreply.github.com>
2021-06-30 14:03:42 -0400

    Fix tests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).end("Hello World 2!");
+  res.status(200).end("Hello World!");
 });
 
 module.exports = app;
